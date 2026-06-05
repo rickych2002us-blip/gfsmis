db04bde4e47b56e6e2c1f1838a6e7b5550a9fa55
Ricky Chan <rchan@gplinc.com>
2026-06-05 09:49:44 -0400

    Add Procfile, gunicorn, psycopg2 for Railway deployment

--- app.py
+++ app.py
@@ -1765,4 +1765,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         seed_database()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=os.environ.get("FLASK_ENV") == "development", host="0.0.0.0", port=port)
