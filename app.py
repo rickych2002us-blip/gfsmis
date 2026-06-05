2d321536730f776f2d34935710a5c1ee70354571
Ricky Chan <rchan@gplinc.com>
2026-06-05 10:26:54 -0400

    seed DB at module load for gunicorn

--- app.py
+++ app.py
@@ -1764,10 +1764,10 @@ def seed_database():
     db.session.commit()
 
 
+with app.app_context():
+    seed_database()
+
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-        seed_database()
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_ENV") != "production"
     app.run(debug=debug, host="0.0.0.0", port=port)
